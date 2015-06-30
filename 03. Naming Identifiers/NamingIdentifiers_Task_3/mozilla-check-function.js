14863cd16a43c265ce7afc2d1ef2e67dd5d45a31
dtopalov <dimiter.topalov79@gmail.com>
2015-06-30 15:49:12 +0300

    Naming Identifiers Homework

---
+++ 03. Naming Identifiers/NamingIdentifiers_Task_3/mozilla-check-function.js
@@ -0,0 +1,10 @@
+function checkIfMozilla() {
+    var myWindow = window,
+        currentBrowser = myWindow.navigator.appCodeName,
+        isMozilla = currentBrowser === "Mozilla";
+    if (isMozilla) {
+        alert("Yes");
+    } else {
+        alert("No");
+    }
+}
