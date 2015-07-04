10925db2c221a9c308d764128e5931aeb8c35bcc
dtopalov <dimiter.topalov79@gmail.com>
2015-07-04 18:19:27 +0300

    Variables, Data, Expressions and Constants HW

---
+++ 05. Variables, Data, Expressions and Constants/Homework/VariablesDataExpressionsHomework/VariablesDataExpressionsHomework/Figure.cs
@@ -0,0 +1,70 @@
+﻿namespace VariablesDataExpressionsHomework
+{
+    using System;
+
+    public class Figure
+    {
+        private double width;
+
+        private double height;
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        /// <exception cref="ArgumentOutOfRangeException" accessor="set">Width should not be negative</exception>
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Width should not be" + " negative");
+                }
+
+                this.width = value;
+            }
+        }
+
+        /// <exception cref="ArgumentOutOfRangeException" accessor="set">Height should not be negative</exception>
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Height should not" + " be negative");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public static Figure GetRotatedFigure(Figure figure, double angleOfTheFigureToBeRotated)
+        {
+            double sinusTimesWidth = Math.Abs(Math.Sin(angleOfTheFigureToBeRotated) * figure.Width);
+            double cosinusTimesWidth = Math.Abs(Math.Cos(angleOfTheFigureToBeRotated) * figure.Width);
+            double sinusTimesHeight = Math.Abs(Math.Sin(angleOfTheFigureToBeRotated) * figure.Height);
+            double cosinusTimesHeigth = Math.Abs(Math.Cos(angleOfTheFigureToBeRotated) * figure.Height);
+
+            double rotatedFigureWidth = cosinusTimesWidth + sinusTimesHeight;
+            double rotatedFigureHeight = sinusTimesWidth + cosinusTimesHeigth;
+
+            Figure rotatedFigure = new Figure(rotatedFigureWidth, rotatedFigureHeight);
+
+            return rotatedFigure;
+        }
+    }
+}
---
+++ 05. Variables, Data, Expressions and Constants/Homework/VariablesDataExpressionsHomework/VariablesDataExpressionsHomework/StatisticsPrinter.cs
@@ -0,0 +1,34 @@
+﻿namespace VariablesDataExpressionsHomework
+{
+    using System;
+    using System.Linq;
+
+    public class StatisticsPrinter
+    {
+        public void PrintStatistics(double[] numbers)
+        {
+            Console.WriteLine("Statistics for {0}:", numbers);
+            double smallestNumber = numbers.Min();
+            this.PrintMin(smallestNumber);
+            double largestNumber = numbers.Max();
+            this.PrintMax(largestNumber);
+            double average = numbers.Average();
+            this.PrintAverage(average);
+        }
+
+        private void PrintMin(double smallestNumber)
+        {
+            Console.WriteLine("The smallest number is {0}.", smallestNumber);
+        }
+
+        private void PrintMax(double largestNumber)
+        {
+            Console.WriteLine("The largest number is {0}.", largestNumber);
+        }
+
+        private void PrintAverage(double average)
+        {
+            Console.WriteLine("The average of the numbers is {0}.", average);
+        }
+    }
+}
